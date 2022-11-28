e0149942e725afdd10a3b2175a8883f808b672c4
Mohammad Amin Ahmadi <amin1382422amin@gmail.com>
2022-11-28 22:16:31 +0330

    Add project files.

---
+++ PrintTheRestOfYear/Program.cs
@@ -0,0 +1,14 @@
+﻿using MD.PersianDateTime;
+//سال را به فرمت میلادی وارد کنید و تقویم ان سال میلادی را به حجری دریافت کنید 
+Console.Write("Inter the first day of the year : ");
+var date = Convert.ToDateTime(Console.ReadLine());
+var persianDate = new PersianDateTime(date);
+var targetYear = persianDate.AddYears(1);
+while (true)
+{
+    if (persianDate == targetYear)
+        break;
+    Console.WriteLine($"year {persianDate.Year} monthe {persianDate.Month}  {persianDate.GetLongDayOfWeekName}  day {persianDate.Day} ");
+    persianDate = persianDate.AddDays(1);
+}
+
